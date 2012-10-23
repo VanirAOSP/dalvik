10c2d6b132804acb479488d5096682416b59caca
PrimeDirective <activethrasher00@gmail.com>
2012-10-23 16:50:11 -0400

    dalvik/vm: build with strict aliasing
    Change-Id: Ib27131f1bf487c1f6bb8c22d066b2cacc527ced5

--- vm/Android.mk
+++ vm/Android.mk
@@ -45,7 +45,7 @@ include $(LOCAL_PATH)/ReconfigureDvm.mk
 # Overwrite default settings
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libdvm
-LOCAL_CFLAGS += $(target_smp_flag)
+LOCAL_CFLAGS += $(target_smp_flag) -fstrict-aliasing -Werror=strict-aliasing
 
 # Define WITH_ADDRESS_SANITIZER to build an ASan-instrumented version of the
 # library in /system/lib/asan/libdvm.so.
